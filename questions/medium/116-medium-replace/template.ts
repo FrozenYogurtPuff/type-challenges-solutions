834ca4d18a039cab2780cd67166cb7df2dc2a21a
FrozenYogurtPuff <frozenyogurtpuff@gmail.com>
2022-02-07 00:52:52 +0800

    feat(medium-116): complete 116-medium-replace

---
+++ questions/medium/116-medium-replace/template.ts
@@ -0,0 +1,6 @@
+type Replace<S extends string, From extends string, To extends string> =
+    S extends `${infer Before}${From}${infer After}` ?
+        From extends '' ?
+            S
+        : `${Before}${To}${After}`
+    : S
