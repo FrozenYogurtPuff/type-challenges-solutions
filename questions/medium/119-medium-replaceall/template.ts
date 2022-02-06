bd7f6538839020ab9d7f38815a200f99d6892fbe
FrozenYogurtPuff <frozenyogurtpuff@gmail.com>
2022-02-07 00:57:39 +0800

    feat(medium-119): complete 119-medium-replaceall

---
+++ questions/medium/119-medium-replaceall/template.ts
@@ -0,0 +1,6 @@
+type ReplaceAll<S extends string, From extends string, To extends string> =
+    S extends `${infer Before}${From}${infer After}` ?
+        From extends '' ?
+            S
+        : `${Before}${To}${ReplaceAll<After, From, To>}`
+    : S
