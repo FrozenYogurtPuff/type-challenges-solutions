5add40c51796fd16a94ce65ac1229ba0ab09c763
FrozenYogurtPuff <frozenyogurtpuff@gmail.com>
2022-02-12 14:46:20 +0800

    feat(medium-4179): update 4179-medium-flip

--- questions/medium/4179-medium-flip/template.ts
+++ questions/medium/4179-medium-flip/template.ts
@@ -1,6 +1,6 @@
 type Flip<T> = {
     [Key in keyof T as
-        T[Key] extends number | string | symbol
+        T[Key] extends number | string | bigint | boolean
             ? `${T[Key]}`
             : never
     ]: Key
