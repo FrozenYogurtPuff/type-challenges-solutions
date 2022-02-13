1a1770dc61e96cf35faae94612a7d83a4850c1a4
FrozenYogurtPuff <frozenyogurtpuff@gmail.com>
2022-02-13 12:26:14 +0800

    feat(hard-6): complete 6-hard-simple-vue

---
+++ questions/hard/6-hard-simple-vue/template.ts
@@ -0,0 +1,11 @@
+// --noImplicitThis
+type Computed<T> = {
+    [P in keyof T]: T[P] extends () => infer V ? V : never
+}
+type Options<D, C, M> = {
+    data: (this: {}) => D,
+    computed: ThisType<D & Computed<C> & M> & C,
+    methods: ThisType<D & Computed<C> & M> & M
+}
+
+declare function SimpleVue<D, C, M>(options: Options<D, C, M>): any
